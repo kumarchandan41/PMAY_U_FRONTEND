4565f85f6c5068fdfd546f768bec588a1e04b3a6
PrabodhMohan <prabodh.bansal7@gmail.com>
2020-02-14 18:52:14 +0530

    report

--- src/app/financeReport/statedisttdity-ata-glance/statedisttdity-ata-glance.component.ts
+++ src/app/financeReport/statedisttdity-ata-glance/statedisttdity-ata-glance.component.ts
@@ -309,7 +309,8 @@ this.SingleStatus6=false;
             if (nameA > nameB) {
               return 1;
             }
-          
+            
+            
             // names must be equal
             return 0;
           });
